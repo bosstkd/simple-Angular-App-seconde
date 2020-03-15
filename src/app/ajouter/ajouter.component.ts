359816b0a8b12564b7697278bb0d713ec97b8469
bosstkd <a-ek@hotmail.fr>
2020-03-15 22:46:13 +0100

    Ajout message sur l'insertion

--- src/app/ajouter/ajouter.component.ts
+++ src/app/ajouter/ajouter.component.ts
@@ -10,27 +10,35 @@ export class AjouterComponent implements OnInit {
 
 
   prd:any;
-  cols: any[];
 
+  
+    status:number;
+    message:string;
+  
   constructor(private cs:CatalogueService, private router:Router) { }
 
   ngOnInit() { }
 
   onSaveProduit(prd){
 
-    this.cs.insertProduit(prd).subscribe(data=>{
-      console.log(data["desig"]);
-      this.router.navigateByUrl("/produits");
-      /*
-      if(prd.desig == data["desig"]){
-        
-      }
-      */
-    },
-      err=> {
-        console.log(err) 
-      }
-      );
+    if(prd.desig.length < 4 || prd.prix < 1 || prd.qte < 1){
+      this.status = 2;
+      this.message = "Merci de rensigner tout les champs svp !!";
+    }else{
+              this.cs.insertProduit(prd).subscribe(data=>{
+              console.log(data["desig"]);
+            //  this.router.navigateByUrl("/produits");
+              this.status = 1;
+              this.message = "Produit ajouter avec succés.";
+             },
+              err=> {
+                console.log(err);
+                this.status = 3;
+                this.message = err;
+              }
+              );
+    }
+   
   }
 
 
